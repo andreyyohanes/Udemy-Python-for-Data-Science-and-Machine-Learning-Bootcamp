2a3b65690b0461d96e0c993f3a3c8850c6a48ba8
Andrey Yohanes <andrey.yohanes@gmail.com>
2021-03-23 23:37:28 +0700

    Create findDog.py

---
+++ 01 Python Crash Course/findDog.py
@@ -0,0 +1,5 @@
+# Create a basic function that returns True if the word 'dog' is contained in the input string.
+# Don't worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization.
+
+def findDog(words):
+    return "dog" in words.lower().split()
